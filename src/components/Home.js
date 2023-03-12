332bb96919bc92568b1b80eb484b5987141304b2
sowmik-sec <sowmik.sec@gmail.com>
2023-03-12 15:52:07 +0600

    deleting a user

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const users = useLoaderData();
+  const [displayUsers, setDisplayUsers] = useState(users);
+  const handleDelete = (user) => {
+    const agree = window.confirm("Are you sure you want to delete?");
+    if (agree) {
+      //   console.log(`deleting item of id ${user._id}`);
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("User deleted successfully.");
+            const remainingUsers = displayUsers.filter(
+              (usr) => usr._id !== user._id
+            );
+            setDisplayUsers(remainingUsers);
+          }
+        });
+    }
+  };
   return (
     <div>
-      {users.map((user) => (
-        <p key={user._id}>{user.name}</p>
+      {displayUsers.map((user) => (
+        <p key={user._id}>
+          {user.name} <button onClick={() => handleDelete(user)}>X</button>
+        </p>
       ))}
     </div>
   );
